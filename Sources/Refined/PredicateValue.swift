ab0df981afe84edf7c9effaef557641ab3e8336e
hectr <h@mrhector.me[core]>
2019-03-20 01:46:54 +0100

    Define PredicateValue protocol

---
+++ Sources/Refined/PredicateValue.swift
@@ -0,0 +1,13 @@
+import Swift
+
+/// Predicate that represents a concrete `Equatable` value.
+public protocol PredicateValue: Predicate where Value: Equatable {
+    associatedtype Value
+    static var value: Value { get }
+}
+
+extension PredicateValue {
+    public static func isValid(value: Value) -> Bool {
+        return value == self.value
+    }
+}
