217aa65e79d03f6e52353587eaf59bd064d1220f
hectr <h@mrhector.me>
2019-08-15 09:09:35 +0200

    Make refined conform to wrappable protocols

---
+++ Sources/Refined/Refined+Wrappable.swift
@@ -0,0 +1,42 @@
+// Generated with gyb. Do not edit.
+
+import Wrapper
+
+/// Refined + CVarArg extension.
+extension Refined: CVarArg where WrappedValue: CVarArg {}
+
+/// Refined + CodingKey extension.
+extension Refined: CodingKey where WrappedValue: CodingKey {}
+
+/// Refined + Comparable extension.
+extension Refined: Comparable where WrappedValue: Comparable {}
+
+/// Refined + CustomDebugStringConvertible extension.
+extension Refined: CustomDebugStringConvertible where WrappedValue: CustomDebugStringConvertible {}
+
+/// Refined + CustomPlaygroundDisplayConvertible extension.
+extension Refined: CustomPlaygroundDisplayConvertible where WrappedValue: CustomPlaygroundDisplayConvertible {}
+
+/// Refined + CustomReflectable extension.
+extension Refined: CustomReflectable where WrappedValue: CustomReflectable {}
+
+/// Refined + CustomStringConvertible extension.
+extension Refined: CustomStringConvertible where WrappedValue: CustomStringConvertible {}
+
+/// Refined + Decodable extension.
+extension Refined: Decodable where WrappedValue: Decodable {}
+
+/// Refined + Equatable extension.
+extension Refined: Equatable where WrappedValue: Equatable {}
+
+/// Refined + LosslessStringConvertible extension.
+extension Refined: LosslessStringConvertible where WrappedValue: LosslessStringConvertible {}
+
+/// Refined + _CVarArgAligned extension.
+extension Refined: _CVarArgAligned where WrappedValue: _CVarArgAligned {}
+
+/// Refined + _CustomPlaygroundQuickLookable extension.
+extension Refined: _CustomPlaygroundQuickLookable where WrappedValue: _CustomPlaygroundQuickLookable {}
+
+/// Refined + __DefaultCustomPlaygroundQuickLookable extension.
+extension Refined: __DefaultCustomPlaygroundQuickLookable where WrappedValue: __DefaultCustomPlaygroundQuickLookable {}
