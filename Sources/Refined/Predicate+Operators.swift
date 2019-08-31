f208cbb7bdcb11062997ce922649ac2c92a69388
hectr <h@mrhector.me>
2019-08-31 19:46:10 +0200

    Replace numeric predicates with operators template

---
+++ Sources/Refined/Predicate+Operators.swift
@@ -0,0 +1,153 @@
+// Generated with gyb. Do not edit.
+
+import Wrapper
+
+// MARK: - LessEqual
+
+/// Predicate that checks if a value is *lessEqual* compared to `PredicateValue`.
+public struct LessEqual<V: PredicateValue>: Predicate where V.Value: LessEqualCompatible {
+    @available(*, unavailable)
+    private init() {}
+
+    public static func isValid(value lhs: V.Value) -> Bool {
+        return lhs <= V.value
+    }
+}
+
+/// Refined's internal typealias of `LessEqual`.
+public typealias _LessEqualPredicate = LessEqual
+
+/// `LessEqual` DSL extension.
+extension LessEqualCompatible {
+    public typealias LessEqual<T: PredicateValue> = Refined<_LessEqualPredicate<T>> where T.Value == Self
+}
+
+/// `And` + `LessEqual` DSL extension.
+extension Refined.And where Refined.Value: LessEqualCompatible {
+    public typealias LessEqual<T: PredicateValue> = Refined._And<_LessEqualPredicate<T>> where T.Value == Refined.Value
+}
+
+/// `Or` + `LessEqual` DSL extension.
+extension Refined.Or where Refined.Value: LessEqualCompatible {
+    public typealias LessEqual<T: PredicateValue> = Refined._Or<_LessEqualPredicate<T>> where T.Value == Refined.Value
+}
+
+// MARK: - Less
+
+/// Predicate that checks if a value is *less* compared to `PredicateValue`.
+public struct Less<V: PredicateValue>: Predicate where V.Value: LessCompatible {
+    @available(*, unavailable)
+    private init() {}
+
+    public static func isValid(value lhs: V.Value) -> Bool {
+        return lhs < V.value
+    }
+}
+
+/// Refined's internal typealias of `Less`.
+public typealias _LessPredicate = Less
+
+/// `Less` DSL extension.
+extension LessCompatible {
+    public typealias Less<T: PredicateValue> = Refined<_LessPredicate<T>> where T.Value == Self
+}
+
+/// `And` + `Less` DSL extension.
+extension Refined.And where Refined.Value: LessCompatible {
+    public typealias Less<T: PredicateValue> = Refined._And<_LessPredicate<T>> where T.Value == Refined.Value
+}
+
+/// `Or` + `Less` DSL extension.
+extension Refined.Or where Refined.Value: LessCompatible {
+    public typealias Less<T: PredicateValue> = Refined._Or<_LessPredicate<T>> where T.Value == Refined.Value
+}
+
+// MARK: - GreaterEqual
+
+/// Predicate that checks if a value is *greaterEqual* compared to `PredicateValue`.
+public struct GreaterEqual<V: PredicateValue>: Predicate where V.Value: GreaterEqualCompatible {
+    @available(*, unavailable)
+    private init() {}
+
+    public static func isValid(value lhs: V.Value) -> Bool {
+        return lhs >= V.value
+    }
+}
+
+/// Refined's internal typealias of `GreaterEqual`.
+public typealias _GreaterEqualPredicate = GreaterEqual
+
+/// `GreaterEqual` DSL extension.
+extension GreaterEqualCompatible {
+    public typealias GreaterEqual<T: PredicateValue> = Refined<_GreaterEqualPredicate<T>> where T.Value == Self
+}
+
+/// `And` + `GreaterEqual` DSL extension.
+extension Refined.And where Refined.Value: GreaterEqualCompatible {
+    public typealias GreaterEqual<T: PredicateValue> = Refined._And<_GreaterEqualPredicate<T>> where T.Value == Refined.Value
+}
+
+/// `Or` + `GreaterEqual` DSL extension.
+extension Refined.Or where Refined.Value: GreaterEqualCompatible {
+    public typealias GreaterEqual<T: PredicateValue> = Refined._Or<_GreaterEqualPredicate<T>> where T.Value == Refined.Value
+}
+
+// MARK: - Greater
+
+/// Predicate that checks if a value is *greater* compared to `PredicateValue`.
+public struct Greater<V: PredicateValue>: Predicate where V.Value: GreaterCompatible {
+    @available(*, unavailable)
+    private init() {}
+
+    public static func isValid(value lhs: V.Value) -> Bool {
+        return lhs > V.value
+    }
+}
+
+/// Refined's internal typealias of `Greater`.
+public typealias _GreaterPredicate = Greater
+
+/// `Greater` DSL extension.
+extension GreaterCompatible {
+    public typealias Greater<T: PredicateValue> = Refined<_GreaterPredicate<T>> where T.Value == Self
+}
+
+/// `And` + `Greater` DSL extension.
+extension Refined.And where Refined.Value: GreaterCompatible {
+    public typealias Greater<T: PredicateValue> = Refined._And<_GreaterPredicate<T>> where T.Value == Refined.Value
+}
+
+/// `Or` + `Greater` DSL extension.
+extension Refined.Or where Refined.Value: GreaterCompatible {
+    public typealias Greater<T: PredicateValue> = Refined._Or<_GreaterPredicate<T>> where T.Value == Refined.Value
+}
+
+// MARK: - Equal
+
+/// Predicate that checks if a value is *equal* compared to `PredicateValue`.
+public struct Equal<V: PredicateValue>: Predicate where V.Value: EqualCompatible {
+    @available(*, unavailable)
+    private init() {}
+
+    public static func isValid(value lhs: V.Value) -> Bool {
+        return lhs == V.value
+    }
+}
+
+/// Refined's internal typealias of `Equal`.
+public typealias _EqualPredicate = Equal
+
+/// `Equal` DSL extension.
+extension EqualCompatible {
+    public typealias Equal<T: PredicateValue> = Refined<_EqualPredicate<T>> where T.Value == Self
+}
+
+/// `And` + `Equal` DSL extension.
+extension Refined.And where Refined.Value: EqualCompatible {
+    public typealias Equal<T: PredicateValue> = Refined._And<_EqualPredicate<T>> where T.Value == Refined.Value
+}
+
+/// `Or` + `Equal` DSL extension.
+extension Refined.Or where Refined.Value: EqualCompatible {
+    public typealias Equal<T: PredicateValue> = Refined._Or<_EqualPredicate<T>> where T.Value == Refined.Value
+}
