c303627eb451a7f5570d32ef3f21c781eb41178c
hectr <h@mrhector.me>
2019-09-01 11:01:37 +0200

    Run `swift test --generate-linuxmain`

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -3,21 +3,6 @@ import XCTest
 import RefinedTests
 
 var tests = [XCTestCaseEntry]()
-
-tests += PredicateTests.allTests()
-tests += RefinedTests.allTests()
-
-tests += LetterTests.allTests()
-tests += LowercaseTests.allTests()
-tests += NumberTests.allTests()
-tests += UppercaseTests.allTests()
-tests += WhitespaceTests.allTests()
-
-tests += CountTests.allTests()
-tests += EmptyTests.allTests()
-
-tests += NotTests.allTests()
-tests += AndTests.allTests()
-tests += OrTests.allTests()
+tests += RefinedTests.__allTests()
 
 XCTMain(tests)
